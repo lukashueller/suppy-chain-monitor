7d2d675c51f773513ddf211823fc133fa9273e87
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2023-04-16 20:42:27 +0200

    redesign Error page

--- src/components/pages/Error/Error.jsx
+++ src/components/pages/Error/Error.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import HeaderNavbar from "../../multiPageComponents/HeaderNavbar/HeaderNavbar";
+import { Row, Typography } from "antd";
 
-const Error = () => <>The page you are looking for does not exist</>;
+const { Title } = Typography;
+
+const Error = () => {
+  return (
+    <Row
+      justify="center"
+      style={{ backgroundColor: "#E0E0E0", height: "100vh" }}
+    >
+      <HeaderNavbar />
+      <Title>The page you are looking for does not exist</Title>
+    </Row>
+  );
+};
 
 export default Error;
