f79ba117d680a04704be2ab6996dcd381cb9e5c4
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2023-04-17 16:53:31 +0200

    start implementing delete button

--- src/components/pages/Modeler/SupplierBox/SupplierBox.jsx
+++ src/components/pages/Modeler/SupplierBox/SupplierBox.jsx
@@ -1,11 +1,33 @@
-import React, { useEffect, useState } from "react";
-import { Card } from "antd";
+import React from "react";
+import { Card, Button, Row, Typography } from "antd";
+
+import { DeleteOutlined } from "@ant-design/icons";
+
+const { Text, Title } = Typography;
 
 const SupplierBox = (props) => {
   const { supplier } = props;
+
+  const handleDeletionClick = () => {
+    console.log("clicked!" + supplier);
+    // TODO: Handle Click in Modelers State
+  };
+
   return (
     <Card type="inner" style={{ width: "100%" }}>
-      {supplier}
+      <Row
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Text strong>{supplier}</Text>
+        <Button
+          icon={<DeleteOutlined />}
+          onClick={() => handleDeletionClick()}
+        />
+      </Row>
     </Card>
   );
 };
