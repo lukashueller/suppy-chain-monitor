a9397ee6479b3ac252ebea328d79e7d4aaecf4fe
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2023-04-18 11:43:46 +0200

    implement drawer opening

--- src/components/pages/Modeler/SupplierBox/SupplierBox.jsx
+++ src/components/pages/Modeler/SupplierBox/SupplierBox.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Card, Button, Row, Typography } from "antd";
+import React, { useState } from "react";
+import { Card, Button, Row, Typography, Drawer, Space } from "antd";
 
 import { DeleteOutlined } from "@ant-design/icons";
 
@@ -7,27 +7,65 @@ const { Text, Title } = Typography;
 
 const SupplierBox = (props) => {
   const { supplier, onSupplierDeletion } = props;
+  const [open, setOpen] = useState(false);
 
   const handleDeletionClick = () => {
     onSupplierDeletion(supplier);
   };
 
-  return (
-    <Card type="inner" style={{ width: "100%" }}>
-      <Row
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
+  const openDrawer = () => {
+    setOpen(true);
+  };
+  const closeDrawer = () => {
+    setOpen(false);
+  };
+
+  const returnDrawer = () => {
+    return (
+      <Drawer
+        title={"More Information about: " + supplier}
+        size="large"
+        onClose={closeDrawer}
+        open={open}
+        extra={
+          <Space>
+            <Button onClick={closeDrawer}>Cancel</Button>
+          </Space>
+        }
       >
-        <Text strong>{supplier}</Text>
-        <Button
-          icon={<DeleteOutlined />}
-          onClick={() => handleDeletionClick()}
-        />
-      </Row>
-    </Card>
+        <p>Some contents...</p>
+      </Drawer>
+    );
+  };
+
+  return (
+    <>
+      <Card
+        type="inner"
+        style={{
+          cursor: "pointer",
+          boxShadow: "0 0 5px rgba(0, 0, 0, 0.2)",
+          width: "100%",
+        }}
+        onClick={openDrawer}
+      >
+        <Row
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text strong>{supplier}</Text>
+          <Button
+            icon={<DeleteOutlined />}
+            onClick={() => handleDeletionClick()}
+          />
+        </Row>
+      </Card>
+
+      {returnDrawer()}
+    </>
   );
 };
 
