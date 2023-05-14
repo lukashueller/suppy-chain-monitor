730f6ed3f721b1c4ca81ff8233b0d4a79ad4afab
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2023-05-14 16:48:44 +0200

    add corporate design

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ConfigProvider } from "antd";
+
 import Home from "./pages/Home/Home";
 import Modeler from "./pages/Modeler/Modeler";
 import SupplierNetwork from "./pages/SupplierNetwork/SupplierNetwork";
@@ -10,17 +12,25 @@ import Signup from "./pages/Signup/Signup";
 import "./App.css";
 
 const App = () => (
-  <BrowserRouter basename="/suppy-chain-monitor">
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="/modeler" element={<Modeler />} />
-      <Route path="/network" element={<SupplierNetwork />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="*" element={<Error />} />
-      {/* <Route path="/somethingwithId/:id" element={somethingwithId} /> */}
-    </Routes>
-  </BrowserRouter>
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: "#64C094",
+      },
+    }}
+  >
+    <BrowserRouter basename="/suppy-chain-monitor">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/modeler" element={<Modeler />} />
+        <Route path="/network" element={<SupplierNetwork />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Error />} />
+        {/* <Route path="/somethingwithId/:id" element={somethingwithId} /> */}
+      </Routes>
+    </BrowserRouter>
+  </ConfigProvider>
 );
 
 export default App;
