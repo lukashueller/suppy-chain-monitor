8d50ccf51a8905d163e3d14a888a1cf0d0836548
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2023-05-24 19:56:09 +0200

    lint fix

--- src/components/pages/Error/Error.jsx
+++ src/components/pages/Error/Error.jsx
@@ -6,10 +6,7 @@ const { Title } = Typography;
 
 const Error = () => {
   return (
-    <Row
-      justify="center"
-      style={{ backgroundColor: "#E0E0E0", height: "100vh" }}
-    >
+    <Row justify="center" style={{ backgroundColor: "#E0E0E0", height: "100vh" }}>
       <HeaderNavbar selectedKey={0} />
       <Title>The page you are looking for does not exist</Title>
     </Row>
